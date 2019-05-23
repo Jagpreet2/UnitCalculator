2ce8d2e85c2226935b747c6721941d730b595d40
Jagpreet2 <47798980+Jagpreet2@users.noreply.github.com>
2019-05-23 09:56:50 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+
+namespace CalcAppTest
+
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void GetAddition_Input4point5and3point5_Returns8point0()
+        {
+
+            //Arrange
+            double number1 = 4.5;
+            double number2 = 3.5;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetAddition_Input3point5and5point8_Returns9point3()
+        {
+
+            //Arrange
+            double number1 = 3.5;
+            double number2 = 5.8;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetAddition_Input1point1and5point8_Returns6point9()
+        {
+
+            //Arrange
+            double number1 = 1.1;
+            double number2 = 5.8;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+    }
+}
